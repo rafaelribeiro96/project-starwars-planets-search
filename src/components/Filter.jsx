4b48cb251471f9016b0815db3d47d093c5de5735
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2022-09-21 13:26:10 -0300

    requisito 6 realizado

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,32 +1,38 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Context from '../context/Context';
 
 function Filters() {
-  const { setFilterByName, setFilterByNumbers } = useContext(Context);
+  const { setFilterByName, setFilterByNumbers, filterByNumbers } = useContext(Context);
   const [filters, setAllFilters] = useState({
-    column: 'population',
-    comparison: 'maior que',
-    number: 0,
+    column: 'population', comparison: 'maior que', number: 0,
   });
+  const [columnFilter, setcolumnFilter] = useState([
+    'population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water',
+  ]);
 
-  const handleChange = ({ target: { name, value } }) => {
-    setAllFilters((prevFilters) => ({
-      ...prevFilters,
-      [name]: value,
-    }));
+  const handleNumericFilter = ({ target: { name, value } }) => {
+    setAllFilters((prevFilters) => ({ ...prevFilters, [name]: value }));
   };
 
-  const handleSubmit = (event) => {
+  const addNumericFilter = (event) => {
     event.preventDefault();
     setFilterByNumbers((prevfilterByNumbers) => [
-      ...prevfilterByNumbers,
-      filters,
-    ]);
+      ...prevfilterByNumbers, filters]);
   };
+
+  useEffect(() => {
+    if (filterByNumbers.length) {
+      const newOptions = columnFilter.filter(
+        (option) => filterByNumbers.some(({ column }) => option !== column),
+      );
+      setcolumnFilter(newOptions); setAllFilters((prevFilters) => ({
+        ...prevFilters, column: newOptions[0] }));
+    }
+  }, [filterByNumbers]);
 
   return (
     <section>
-      <form onSubmit={ handleSubmit }>
+      <form onSubmit={ addNumericFilter }>
         <input
           type="text"
           data-testid="name-filter"
@@ -39,13 +45,11 @@ function Filters() {
             value={ filters.column }
             data-testid="column-filter"
             id="column-filter"
-            onChange={ handleChange }
+            onChange={ handleNumericFilter }
           >
-            <option value="population">population</option>
-            <option value="orbital_period">orbital_period</option>
-            <option value="diameter">diameter</option>
-            <option value="rotation_period">rotation_period</option>
-            <option value="surface_water">surface_water</option>
+            {columnFilter.map((columnOption) => (
+              <option key={ columnOption } value={ columnOption }>{columnOption}</option>
+            ))}
           </select>
         </label>
         <label htmlFor="comparison-filter">
@@ -55,7 +59,7 @@ function Filters() {
             value={ filters.comparison }
             data-testid="comparison-filter"
             id="comparison-filter"
-            onChange={ handleChange }
+            onChange={ handleNumericFilter }
           >
             <option value="maior que">maior que</option>
             <option value="menor que">menor que</option>
@@ -68,7 +72,7 @@ function Filters() {
           name="number"
           data-testid="value-filter"
           value={ filters.number }
-          onChange={ handleChange }
+          onChange={ handleNumericFilter }
         />
         <button data-testid="button-filter" type="submit">Filtrar</button>
       </form>
