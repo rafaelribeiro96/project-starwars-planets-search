f607a6c01870c30d5c787378273d21c9fb548252
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2022-09-21 16:29:57 -0300

    requisito 5 e 8 realizados

---
+++ src/helpers/constants.js
@@ -0,0 +1,4 @@
+export const COLUMN_FILTER = 'column-filter';
+export const COMPARISON_FILTER = 'comparison-filter';
+export const VALUE_FILTER = 'value-filter';
+export const BUTTON_FILTER = 'button-filter';
